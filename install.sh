203f8edd7d459aba9bc933c2ee26746eca7c3256
ryone9re <48243891+ryone9re@users.noreply.github.com>
2022-09-14 17:48:02 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-mkdir -p ${HOME}/.config
+pip3 install --user online-judge-tools
+npm install --location=global atcoder-cli
 ln -snfv ${PWD}/atcoder-cli-nodejs ${HOME}/.config/atcoder-cli-nodejs
+acc login && oj login https://atcoder.jp
