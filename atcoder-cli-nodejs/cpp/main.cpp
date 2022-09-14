e99047c7f7aac03c5df3dd98337dc01b1140e602
ryone9re <48243891+ryone9re@users.noreply.github.com>
2022-09-14 17:39:00 +0900

    Create main.cpp

---
+++ atcoder-cli-nodejs/cpp/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC target("avx")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+
+int main()
+{
+    /* code */
+
+    /* end */
+    return (0);
+}
