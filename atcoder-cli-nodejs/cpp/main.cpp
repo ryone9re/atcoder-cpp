1cb625c94408ea8b8d2471394524314ad4ab823a
ryone9re <48243891+ryone9re@users.noreply.github.com>
2022-11-02 20:18:05 +0900

    Update main.cpp

--- atcoder-cli-nodejs/cpp/main.cpp
+++ atcoder-cli-nodejs/cpp/main.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using ull = unsinged long long;
 
 #define ppc(x) __builtin_popcount(x)
 
 int main()
 {
-    /* code */
+    /* code here */
 
     /* end */
     return (0);
