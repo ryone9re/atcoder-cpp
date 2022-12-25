8789ec10593f171e740831bb304789e98eadf9d9
ryone9re <ryoichi.kurimoto@gmail.com>
2022-12-25 18:24:22 +0900

    fix iroiro

--- atcoder-cli-nodejs/cpp/main.cpp
+++ atcoder-cli-nodejs/cpp/main.cpp
@@ -1,14 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+#pragma GCC target("avx")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+
 using ll = long long;
-using ull = unsinged long long;
+using ull = unsigned long long;
+
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
 
 #define ppc(x) __builtin_popcount(x)
 
-int main()
-{
-    /* code here */
-
-    /* end */
-    return (0);
+int main() {
+  // code
+  return (0);
 }
